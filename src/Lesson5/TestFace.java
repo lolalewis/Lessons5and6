5e2ee07a4d1d8166ef573ea763c1ef269ed3c633
lola1590 <lola1590@ISS2TWRKH2.scdsb.pri>
2019-09-30 12:00:09 -0400

    updated from school

---
+++ src/Lesson5/TestFace.java
@@ -0,0 +1,39 @@
+
+package Lesson5;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class TestFace {
+    
+    private int xPos, yPos, diameter;
+    private Color color;
+    private Graphics g;
+    
+    public TestFace (Graphics g, int x, int y){
+        xPos = x;
+        yPos=y;
+        //let the "g" property = the "g" param
+        this.g = g;
+        //default size and color
+        diameter = 100;
+        color=Color.red;
+    }
+    public void draw(){
+        drawHead();
+        drawEyes();
+        drawMouth();
+    }
+    private void drawHead(){
+        g.fillOval(xPos, yPos, xPos, yPos);
+       
+    }
+    private void drawEyes (){
+       g.fillOval(xPos, yPos, xPos, yPos);
+}
+    private void drawMouth(){
+        g.drawLine(xPos, yPos, xPos, yPos);
+    }
+    
+}
